1198fe707bbeeb57277509566cd0aac55dbf13f4
LiGHTiNiNG <123617404+Lightining20@users.noreply.github.com>
2024-03-30 00:14:08 +0530

    s

--- src/app.js
+++ src/app.js
@@ -9,5 +9,17 @@ app.use(
   })
 );
 app.use(express.static("public"));
+app.use("/users", (req, res) => {
+  // This function will be called when a GET request is made to '/api/users'
+  // You can perform any logic here, such as querying a database, and then send a response back
+
+  // For example, send a JSON response with dummy data
+  res.json({
+    users: [
+      { id: 1, name: "John" },
+      { id: 2, name: "Jane" },
+    ],
+  });
+});
 
 export { app };
